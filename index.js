47c0513f193802b43a2674da51506201f5373803
hafeezur-rahman-au3 <52407289+hafeezur-rahman-au3@users.noreply.github.com>
2019-09-12 18:47:25 +0530

    Edit and Update issue Fixed

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ var auth = require("./routes/auth.js");
 var checkToken = require("./middleware/check-authToken");
 var db;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(bodyParser.urlencoded());
 
@@ -29,7 +31,7 @@ app.set("view engine", "hbs");
 var url = "mongodb://localhost:27017";
 
 app.locals.db;
-
+// app.locals.faqdb;
 mongoClient.connect(
     url, {
         useNewUrlParser: true
@@ -37,11 +39,12 @@ mongoClient.connect(
     function(err, client) {
         if (err) throw err;
         db = client.db("school"); //will change db name, when it is created
-        db = client.db("faq"); 
+        // faqdb = client.db("faq");
     }
 );
 
-app.locals.ObjectId = require("mongodb").ObjectID;
+app.locals.ObjectId;
+ObjectId = require("mongodb").ObjectID;
 
 app.use(
     session({
@@ -94,20 +97,20 @@ app.get("/profile", checkToken, (req, res) => {
     });
 });
 
-app.post('/search', function (req, res) {
-  // console.log(JSON.stringify(req.body));
-  searchResult.push(req.body);
-  var airlines = searchResult[0];
-  var newAirlines = airlines.Carriers;
-  // console.log(airlines);  
-  // console.log(newAirlines);
-  var newAirlines = airlines.Carriers;
-  newAirlines.forEach((element, index, array) => {
-    airlineName.push(element.Name);
-  });
-  console.log(airlineName);
-  // console.log(JSON.stringify(searchResult));
-  res.redirect('/');
+app.post('/search', function(req, res) {
+    // console.log(JSON.stringify(req.body));
+    searchResult.push(req.body);
+    var airlines = searchResult[0];
+    var newAirlines = airlines.Carriers;
+    // console.log(airlines);  
+    // console.log(newAirlines);
+    var newAirlines = airlines.Carriers;
+    newAirlines.forEach((element, index, array) => {
+        airlineName.push(element.Name);
+    });
+    console.log(airlineName);
+    // console.log(JSON.stringify(searchResult));
+    res.redirect('/');
 });
 
 //faq section starts here
@@ -119,120 +122,146 @@ app.post('/search', function (req, res) {
 
 //home page
 app.get("/faq", function(req, res) {
-    db.collection("addfaq").find().toArray(function(err,result){
-        if(err) throw err;
-            res.render("displayfaq",{
-                data : result,
-                title : "List of FAQ",
-                script: "/create.js"
-            });
-      }); 
-});
-
-
-//add the faq
-app.get('/faq/create', function (req, res) {
-    res.render('create',{
-        title:"Add your FAQ"
+    db.collection("addfaq").find().toArray(function(err, result) {
+        if (err) throw err;
+        res.render("displayfaq", {
+            data: result,
+            title: "List of FAQ",
+            script: "/create.js"
+        });
     });
 });
 
 
-app.post("/addfaq", function(req,res){
+//add the faq
+app.get('/faq/create', function(req, res) {
+    res.render('create', {
+        title: "Add your FAQ"
+    });
+});
+
+
+app.post("/addfaq", function(req, res) {
     db.collection("addfaq").insertOne(req.body);
     console.log(req.body);
     res.redirect("/listfaq");
 });
 
 //displaying the FAQ
-app.get("/listfaq", function(req,res){
-    db.collection("addfaq").find().toArray(function(err,result){
-        if(err) throw err;
-            res.render("listfaq",{
-                data : result,
-                title : "FAQ",
-                script: "/create.js"
-            });
-    }); 
+app.get("/listfaq", function(req, res) {
+    db.collection("addfaq").find().toArray(function(err, result) {
+        if (err) throw err;
+        res.render("listfaq", {
+            data: result,
+            title: "FAQ",
+            script: "/create.js"
+        });
+    });
 });
 //for string 
 
 
+app.get('/editFaqByid/:id', (req, res) => {
+    db.collection("addfaq").findOne({
+        _id: ObjectId(req.params.id)
+    }, (err, result) => {
+        if (err) throw err;
+        res.json(result)
+        console.log(result)
+    })
+})
 
-app.get('/editfaq/:id', function (req, res) {
-    var faq= {
+app.put('/updateFaq/:id', (req, res) => {
+    db.collection('addfaq').findOneAndUpdate({
+        _id: ObjectId(req.params.id)
+    }, {
+        $set: (req.body)
+    }, (err, result) => {
+        if (err) throw err;
+        res.json(result)
+    })
+})
+
+
+app.get('/editfaq/:id', function(req, res) {
+    var faq = {
         question: req.body.question,
         answer: req.body.answer
     };
-db.collection("addfaq").find({_id : req.params.id}).toArray(function(err,result){
-    if(err) throw err;
-        res.render("editfaq",{
-            data : result,
-            title : "Edit FAQ",
-            question : req.body.question,
-            answer : req.body.answer
+    db.collection("addfaq").find({
+        _id: req.params.id
+    }).toArray(function(err, result) {
+        if (err) throw err;
+        res.render("editfaq", {
+            data: result,
+            title: "Edit FAQ",
+            question: req.body.question,
+            answer: req.body.answer
         });
-    }); 
+    });
 });
 
 //Update part :
-app.put('/listfaq/:id', function(req,res){
-    db.collection('addfaq').findOne({_id: req.params.id}).toArray(function(err,result){
-        if(error) throw err
-//        .then( db.collection('addfaq') => {
-//             addfaq.question = req.body.question;
-//            addfaq.answer = req.body.answer;
-//        })
+app.put('/listfaq/:id', function(req, res) {
+    db.collection('addfaq').findOne({
+        _id: req.params.id
+    }).toArray(function(err, result) {
+        if (error) throw err
+            //        .then( db.collection('addfaq') => {
+            //             addfaq.question = req.body.question;
+            //            addfaq.answer = req.body.answer;
+            //        })
     })
 });
 
 
 //delete process
-app.delete("/listfaq/:_id", function(req,res){
-    db.collection('addfaq').deleteOne({_id:require('mongodb').ObjectID(req.params._id
-    )}, function(error,result){
-        if(error) 
-    throw error
-            res.redirect('/listfaq');
+app.delete("/listfaq/:_id", function(req, res) {
+    db.collection('addfaq').deleteOne({
+        _id: require('mongodb').ObjectID(req.params._id)
+    }, function(error, result) {
+        if (error)
+            throw error
+        res.redirect('/listfaq');
     });
 });
 //faq section ends here
 //faq login part starts
-var adminLog = [
-    {
+var adminLog = [{
         username: "admin",
         email: "admin@gmail.com",
         password: "admin"
     },
-    
+
 ];
-app.post("/login",function(req,res){
-    for( var i=0; i<adminLog.length; i++){
-        if(req.body.email == adminLog[i].email && req.body.password == adminLog[i].password ){
-            req.session.loggedIn=true;
+app.post("/login", function(req, res) {
+    for (var i = 0; i < adminLog.length; i++) {
+        if (req.body.email == adminLog[i].email && req.body.password == adminLog[i].password) {
+            req.session.loggedIn = true;
             req.session.username = adminLog.username;
         }
     }
     res.redirect("/listfaq");
 });
-app.get("/admin", function(req,res){
-        if(req.session.loggedIn == true){
-            res.render('login');
-        }
-        else{
-            res.redirect("/listfaq");
-        }
+app.get("/admin", function(req, res) {
+    if (req.session.loggedIn == true) {
+        res.render('login');
+    } else {
+        res.redirect("/listfaq");
+    }
 });
 
-app.get("/adminlog",function(req,res){
-     res.sendFile('/public/login.html', { root: __dirname });
+app.get("/adminlog", function(req, res) {
+    res.sendFile('/public/login.html', {
+        root: __dirname
+    });
 })
 
-app.get("/adminlogout",function(req,res){
-    req.session.destroy();
-    res.redirect("/faq");
-})
-//faq login part ends here
+app.get("/adminlogout", function(req, res) {
+        req.session.destroy();
+        res.redirect("/faq");
+    })
+    //faq login part ends here
 
 //Myaccount--works
 // app.get("/myaccount", checkToken, (req, res) => {
@@ -250,42 +279,46 @@ app.get("/adminlogout",function(req,res){
 
 
 app.get("/myaccount", checkToken, (req, res) => {
-    db.collection('users').find().toArray(function(error,result){
-        if(error)
-        throw error;
-    res.render('myAcc.hbs', ({
-        title: "Account Details",
-        data: result,
-        script:'/script.js',
+    db.collection('users').find().toArray(function(error, result) {
+        if (error)
+            throw error;
+        res.render('myAcc.hbs', ({
+            title: "Account Details",
+            data: result,
+            script: '/script.js',
 
 
-    }));
-});
+        }));
+    });
 
 
 })
 
 hbs.registerHelper('checked', function(value, test) {
     if (value == undefined) return '';
-    return value==test ? 'checked' : '';
+    return value == test ? 'checked' : '';
 });
 
 
-app.put('/myaccount/acc',checkToken, (req, res) => {
-var proId=req.userData._id
-    var updProfile= req.body;
-    var objectId=require('mongodb').ObjectId
-    db.collection('users').update({"_id": new objectId(proId)},{$set: updProfile},function(error,result){
-        if(error)
-        throw error;
+app.put('/myaccount/acc', checkToken, (req, res) => {
+    var proId = req.userData._id
+    var updProfile = req.body;
+    var objectId = require('mongodb').ObjectId
+    db.collection('users').update({
+        "_id": new objectId(proId)
+    }, {
+        $set: updProfile
+    }, function(error, result) {
+        if (error)
+            throw error;
         console.log(result);
         // res.json(result);
-    
+
+
+    })
+
 
 })
-
-
-}) 
 
 
 app.listen(3000, function() {
